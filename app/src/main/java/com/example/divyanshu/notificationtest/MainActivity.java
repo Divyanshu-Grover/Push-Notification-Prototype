3801c492223d9b72b2614789c5c1911521f3f084
Divyanshu-Grover <divyanshu.grover@gmail.com>
2016-07-27 01:42:50 +0530

    Upload commit

---
+++ app/src/main/java/com/example/divyanshu/notificationtest/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.divyanshu.notificationtest;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    NotificationManager nm;
+    Notification notify;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        notify = new Notification(R.drawable.ic_launcher,"Meeting",System.currentTimeMillis());
+        Intent i = new Intent(getApplicationContext(),second.class);
+        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(),2,i,0);
+        notify.setLatestEventInfo(getApplicationContext(),"Xyz","Wait...",pi);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void showme(View view){
+        nm.notify(3,notify);
+
+    }
+}
